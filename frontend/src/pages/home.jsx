6a6b5faa3b513b713e08018b59766614f654e298
ShlokM08 <shlok21421@iiitd.ac.in>
2025-07-14 23:13:39 +0300

    initialized useState in searchbox

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,7 +1,13 @@
 import MovieCard from "../components/MovieCard"
+import { useState } from "react"
 
 
 function Home(){
+  const[searchQuery,setSearchQuery]=useState("")
+
+
+
+
     const movies=[
         {id:1,title:"John Wick",release_date:"2000"},
                 {id:1,title:"CIA",release_date:"2025"},
@@ -13,8 +19,12 @@ function Home(){
     }
     return <div className="Home">
         <form onSubmit={handleSearch} className="searh-form">
-            <input type="text" placeholder="Search for Movies..." className="Search Input"/>
+            <input type="text" placeholder="Search for Movies..." className="Search Input"
+            value={searchQuery}
+            onChange={(e)=> setSearchQuery(e.target.value)}/>
+            
             <button type="Submit" className="search-button">Search</button>
+            
         </form>
       <div className="movies-grid">
         {movies.map((movie) => (
