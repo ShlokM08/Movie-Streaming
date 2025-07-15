4acb45215c312a9c10d5ba8d1d1104beabf43dc5
ShlokM08 <shlok21421@iiitd.ac.in>
2025-07-15 15:26:50 +0300

    setup page routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,25 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import MovieCard from './components/MovieCard'
 import Home from "./pages/home"
+import Favorites from './pages/Favorites'
 function App() {
   const movieNumber=3
 
   return (
-    <>
-   <Home/>
+   <main className='main-content'>
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path ="/favorites" element={<Favorites/>}/>
+
+    </Routes>
+   </main>
 
     
-  </>
+  
   )
 }
 
