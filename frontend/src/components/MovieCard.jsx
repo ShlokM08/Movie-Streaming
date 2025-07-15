9311c35db40b077ae179bc8d1386c9d09233b3d5
ShlokM08 <shlok21421@iiitd.ac.in>
2025-07-15 20:23:12 +0300

    debugged to add fit movie covers perfectly into boxes

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,30 +1,35 @@
-import "../css/MovieCard.css"
-function MovieCard({movie}){
+// src/components/MovieCard.jsx
+import React from 'react';
+import '../css/MovieCard.css';
 
-    function onFavClick(){
-        alert("clicked")
+function MovieCard({ movie }) {
+  function onFavClick() {
+    alert('clicked');
+  }
 
-    }
-    return (
-        <div className="movie-card">
-            <div className="movie-poster"></div>
-            <img
-  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-  alt={movie.title}
-/>
-            <div className="movie-overlay">
-                <button className="favorite-btn" onClick={onFavClick}>💗</button>
-                
-            </div>
-            <div className="movie-info">
-                <h3>{movie.title}</h3>
-                <p>{movie.release_date?.split("-")[0] }</p>
-                
-            </div>
-
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
+        <div className="movie-overlay">
+          <button
+            className="favorite-btn"
+            onClick={onFavClick}
+          >
+            ♥
+          </button>
         </div>
-    )
+      </div>
 
-
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date?.split('-')[0]}</p>
+      </div>
+    </div>
+  );
 }
-export default MovieCard
+
+export default MovieCard;
