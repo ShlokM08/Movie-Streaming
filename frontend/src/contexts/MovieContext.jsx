524851866c35131638d8fe7234c7c958a954dc73
ShlokM08 <shlok21421@iiitd.ac.in>
2025-07-15 22:37:41 +0300

    made favorite page remove, add and checking if fav

--- frontend/src/contexts/MovieContext.jsx
+++ frontend/src/contexts/MovieContext.jsx
@@ -5,4 +5,39 @@ const MovieContext=createContext()
 
 export const useMovieContext=()=>useContext(MovieContext)
 
-export const MovieProvider=()=>{}
+export const MovieProvider=({children})=>{
+    const [favorites,setFavorites]=useState([])
+
+    useEffect(()=>{
+        const storedFavs=localStorage.getItem("favorites")
+        if(storedFavs) setFavorites(JSON.parse(storedFavs))
+    },[])
+
+    useEffect(()=>{
+        localStorage.setItem('favorites',JSON.stringify(favorites))
+    },[favorites])
+
+    const addToFavorites =(movie)=>{
+        
+        setFavorites(prev=>[...prev,movie])
+
+    }
+
+    const removeFromFavorites =(movieid)=>{
+        setFavorites(prev=>prev.filter(movie=>movie.id!==movieid))
+    }
+
+    const isFavorite=(movieid)=>{
+        return favorites.some(movie=>movie.id===movieid)
+    }
+
+    const value={
+        favorites,addToFavorites,removeFromFavorites,isFavorite
+    }
+
+
+    return <MovieContext.Provider value={value}>
+        {children}
+    </MovieContext.Provider>
+
+}
